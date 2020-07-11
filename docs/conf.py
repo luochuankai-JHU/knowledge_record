b6a0944b5c23f0c321f0f094e71b703fd0f08f17
luochuankai-JHU <cluo16@jh.edu>
2020-07-11 17:05:54 +0800

    indexrst

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = '2020, Chuankai Luo'
 author = u'Chuankai Luo'
 
 # The short X.Y version
-# version = u''
+version = u''
 # # The full version, including alpha/beta/rc tags
-# release = u'0.1.1'
+release = u'洛传凯'
 
 
 # -- General configuration ---------------------------------------------------
