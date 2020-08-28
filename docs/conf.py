996cbd81af46698de44ab1bf36a0821289820c1c
luochuankai-JHU <cluo16@jh.edu>
2020-08-28 16:41:39 +0800

    longtermlstm

--- docs/conf.py
+++ docs/conf.py
@@ -68,9 +68,9 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
-    # "navigation_depth": 2,
-    "collapse_navigation": True,
-    "navigation_depth": 5,
+    "navigation_depth": 2,
+    "collapse_navigation": False,
+    "navigation_depth": 3,
 }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
