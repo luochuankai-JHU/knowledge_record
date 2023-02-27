989b874bc03b7d9869e96b3ee63c1ee9c6f6e4fc
luochuankai-JHU <cluo16@jh.edu>
2023-02-27 16:51:18 +0800

    remove trash

--- docs/conf.py
+++ docs/conf.py
@@ -75,3 +75,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+body = {
+    "body_max_width ": 1000,
+}
